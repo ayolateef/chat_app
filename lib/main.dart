29926282a747d1a4542875e7dbbc9d3dd26d1fa6
Abdullateef Salaudeen Ayodele <abdullateefayodele@gmail.com>
2023-10-12 19:16:55 +0100

    Resolve pubspec.lock conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:chat_app_test/Pages/register.dart';
 import 'package:chat_app_test/Services/auth/auth_gate.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
